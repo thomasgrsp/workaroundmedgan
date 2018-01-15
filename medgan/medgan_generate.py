0ab805af83e3cd7d34421c1bad0fda6de725a4a8
GERSPACHER Thomas <thomas.gerspacher@gmail.com>
2018-01-15 01:57:11 +0100

    Initialisation of GIT

---
+++ medgan/medgan_generate.py
@@ -0,0 +1,27 @@
+from medgan import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parse_arguments(parser)
+
+    data = np.load(args.data_file)
+    inputDim = data.shape[1]
+
+    mg = Medgan(dataType=args.data_type,
+                inputDim=inputDim,
+                embeddingDim=args.embed_size,
+                randomDim=args.noise_size,
+                generatorDims=args.generator_size,
+                discriminatorDims=args.discriminator_size,
+                compressDims=args.compressor_size,
+                decompressDims=args.decompressor_size,
+                bnDecay=args.batchnorm_decay,
+                l2scale=args.L2)
+
+    # To generate synthetic data using a trained model:
+    # Comment the train function above and un-comment generateData function below.
+    # You must specify "--model_file" and "<out_file>" to generate synthetic data.
+    mg.generateData(nSamples=494,
+                modelFile=args.model_file,
+                batchSize=args.batch_size,
+                outFile=args.out_file)
---
+++ medgan/medgan_train.py
@@ -0,0 +1,30 @@
+from medgan import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parse_arguments(parser)
+
+    data = np.load(args.data_file)
+    inputDim = data.shape[1]
+
+    mg = Medgan(dataType=args.data_type,
+                inputDim=inputDim,
+                embeddingDim=args.embed_size,
+                randomDim=args.noise_size,
+                generatorDims=args.generator_size,
+                discriminatorDims=args.discriminator_size,
+                compressDims=args.compressor_size,
+                decompressDims=args.decompressor_size,
+                bnDecay=args.batchnorm_decay,
+                l2scale=args.L2)
+
+    mg.train(dataPath=args.data_file,
+             modelPath=args.model_file,
+             outPath=args.out_file,
+             pretrainEpochs=args.n_pretrain_epoch,
+             nEpochs=args.n_epoch,
+             discriminatorTrainPeriod=args.n_discriminator_update,
+             generatorTrainPeriod=args.n_generator_update,
+             pretrainBatchSize=args.pretrain_batch_size,
+             batchSize=args.batch_size,
+             saveMaxKeep=args.save_max_keep)
