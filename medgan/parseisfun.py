a986a9314bf8505613f94c83133e97d41f83d8c0
GERSPACHER Thomas <thomas.gerspacher@gmail.com>
2018-01-18 14:36:25 +0100

    add chi square test

--- medgan/parseisfun.py
+++ medgan/parseisfun.py
@@ -1,9 +1,7 @@
 import argparse
 
 def parse_arguments(parser):
-	def parse_dims(s):
-		dims = s.split(',')
-		return tuple(map(int, dims))
+
 	parser.add_argument('--tuple', help="Coordinate", dest="cord", type=coords)
 	args = parser.parse_args()
 	return args
